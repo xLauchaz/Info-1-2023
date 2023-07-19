efc199923a2324e5ba9fb8327d5771eccb149292
Rodriguez, Lautaro David <99101061+xLauchaz@users.noreply.github.com>
2023-07-18 21:24:32 -0300

    TP6

---
+++ TP6/area_y_perimetro_rectangulo.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+//Prototipos
+float calcularArea(float ,float );
+float calcularPerimetro(float ,float );
+float imprimirResultados(float, float);
+//Función principal
+int main(void){
+    //Declaración de variables
+    float base, altura;
+    do
+    {
+        printf("Ingrese la longitud del rectángulo: ");
+        scanf("%f", &base);
+        printf("Ingrese la altura del rectángulo: ");
+        scanf("%f", &altura);
+        if (base < 0 || altura < 0)
+        {
+            printf("\x1b[31m""Uno o más valores ingresados son negativos, por favor ingrese valores positivos.\n""\x1b[0m");
+        }
+        
+    } while (base <= 0 || altura <= 0);
+    imprimirResultados(calcularArea(base,altura),calcularPerimetro(base,altura));
+}
+//Funcion que calcula el area
+float calcularArea(float b, float h){
+    return b*h;
+}
+//Funcion que calcula el perimetro
+float calcularPerimetro(float b, float h){
+    return 2*(b+h);
+}
+//Funcion que imprime los resultados
+float imprimirResultados(float a, float p){
+    printf("El área del rectángulo es: %.2f\n", a);
+    printf("El perímetro del rectángulo es: %.2f\n", p);   
+}
