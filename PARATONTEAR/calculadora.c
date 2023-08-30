07558919ac6bb4f102b198a11a5413ebbc6abff4
Rodriguez, Lautaro David <99101061+xLauchaz@users.noreply.github.com>
2023-08-30 19:04:46 -0300

    calculadora

--- PARATONTEAR/calculadora.c
+++ PARATONTEAR/calculadora.c
@@ -3,9 +3,12 @@ int ingreso(void);
 int seleccion(int);
 int suma(void);
 int resta(void);
-void result(int,int);
+int mult(void);
+float div(void);
+void result(float,int);
 int main(void){
-    int op,resp;
+    int op;
+    float resp;
     op = ingreso();
     resp = seleccion(op);
     result(resp, op);
@@ -25,7 +28,7 @@ int ingreso(void){
     return op;
 }
 int seleccion(int a){
-    int b;
+    float b;
     switch (a)
     {
     case 1:
@@ -34,6 +37,12 @@ int seleccion(int a){
     case 2:
         b = resta();
         break;
+    case 3:
+        b = mult();
+        break;
+    case 4:
+        b = div();
+        break;
     default:
         break;
     }
@@ -41,9 +50,9 @@ int seleccion(int a){
 }
 int suma(void){
     int total = 0, num = 0;
+    printf("Ingrese los numeros a sumar, -1 para terminar: ");
     while(num != -1)
     {
-        printf("Ingrese los numeros a sumar, -1 para terminar: ");
         scanf("%d",&num);
         if(num != -1){
         total += num;
@@ -53,9 +62,9 @@ int suma(void){
 }
 int resta(void){
     int total = 0, num = 0, cont = 0;
+    printf("Ingrese los numeros a restar, -1 para terminar: ");
     while (num != -1)
     {
-        printf("Ingrese los numeros a restar, -1 para terminar: ");
         scanf("%d",&num);
         if(num != -1){
             if(cont == 0){
@@ -68,14 +77,49 @@ int resta(void){
     }
     return total;
 }
-void result(int x,int y){
+int mult(void){
+    int total = 1, num = 0;
+    printf("Ingrese los numeros a multiplicar, -1 para terminar: ");
+    while(num != -1)
+    {
+        scanf("%d",&num);
+        if(num != -1){
+        total *= num;
+        }
+    }
+    return total;
+}
+float div(void){
+    int total = 0, num = 0, cont = 0;
+    printf("Ingrese los numeros a dividir, -1 para terminar: ");
+    while (num != -1)
+    {
+        scanf("%d",&num);
+        if(num != -1){
+            if(cont == 0){
+                total = num;
+            }else{
+                total /= num;
+            }
+            cont++;
+        }
+    }
+    return total;
+}
+void result(float x,int y){
     switch (y)
     {
     case 1:
-        printf("El resultado de la Suma es: %d\n", x);
+        printf("El resultado de la Suma es: %.f\n", x);
         break;
     case 2:
-        printf("El resultado de la Resta es: %d\n", x);
+        printf("El resultado de la Resta es: %.f\n", x);
+        break;
+    case 3:
+        printf("El resultado de la Multiplicacion es: %.f\n", x);
+        break;
+    case 4:
+        printf("El resultado de la Division es: %.2f\n", x);
         break;
     default:
         break;
