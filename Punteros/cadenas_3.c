2e905b3c405b7854f16c1634229dbee1513d258c
Rodriguez, Lautaro David <99101061+xLauchaz@users.noreply.github.com>
2023-09-12 15:36:16 -0300

    cadenas de caracteres con punteros

---
+++ Punteros/cadenas_3.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void){
+    char* frases[] = {
+        "El y su familia cantaban y bailaban...",
+        "Podria haber hecho dulces.",
+        "La musica habia destrozado a su familia",
+        "Eres músico, ¿No?",
+        "Me vas a meter en problemas, chico",
+        "Hora del espectaculo, chamaco",
+    };
+
+    printf("%s\n", frases[3]);
+
+    return 0;
+}
